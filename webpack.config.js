f04c8270008e83f0b4102d17eb462298ccdf9c79
Carlos Filoteo <c.filoteo12@gmail.com>
2020-06-24 10:38:29 -0600

    Add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,30 @@ module.exports = (webpackConfigEnv) => {
     module: {
       rules: [
         {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
           test: /\.(png|svg|jpg|gif)$/,
           use: ["file-loader"],
         },
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: ["file-loader"],
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "fonts/[name].[ext]",
+              },
+            },
+          ],
         },
       ],
     },
